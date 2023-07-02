40b092c5deeecefdab83847d24c6e9963462253f
Wookie69Land <lukaskraj@gmail.com>
2023-07-02 17:14:08 +0200

    FitUbiPlan alert

---
+++ fitubi_django/fitubi/static/plan.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const autoPlanA = document.querySelector('#autoPlan');
+
+    autoPlanA.addEventListener("click", function () {
+        window.alert("This feature is not ready yet. Expect unexpected :).")
+        });
+})
